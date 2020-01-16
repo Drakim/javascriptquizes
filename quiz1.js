8e8cf237f8d18add78a90b5586d32caa5a630d26
Kim Lindblom <kimrobinlindblom@gmail.com>
2020-01-16 14:59:20 +0100

    Scrambled the dates

--- quiz1.js
+++ quiz1.js
@@ -1,8 +1,8 @@
 const dates = [
+    new Date('08 October 2014 15:18 UTC'),
     new Date('05 October 2011 18:50 UTC'),
-    new Date('04 Feburary 2013 12:10 UTC'),
     new Date('11 January 2014 11:23 UTC'),
-    new Date('08 October 2014 15:18 UTC')
+    new Date('04 Feburary 2013 12:10 UTC')
 ];
 
 
